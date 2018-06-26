9e8f25981c6b7c5eab52de20d7e5090d777f49e0
Nicole Elgueta <nicole.m.elgueta@gmail.com>
2018-06-26 22:47:06 +0200

    segunda parte

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const car = [['make', 'Ford'], ['model', 'Mustang'], ['year', 1964]];
+/*const car = [['make', 'Ford'], ['model', 'Mustang'], ['year', 1964]];
 const student = [['name','nicole'],['age','36'],['city','villa alemana']];
-const fruits =[['apple','red'],['watermellon','green n pink'], ['limon', 'yellow']];
+const fruits =[['apple','red'],['watermellon','green n pink'], ['limon', 'yellow']];*/
 
 /*let object = {};
 object['make'] = 'ford';
@@ -8,7 +8,7 @@ object['model'] = 'mustang';
 object['year'] = '1964';**/
 //console.log(object);
 
-let arrayToObject = function (array2D) {
+/*let arrayToObject = function (array2D) {
     let newObject = {};
     for(let i=0; i < array2D.length ;i++){
        // nuevo Objeto va ser newObject[propiedades[i][0]]= valores[i][1]
@@ -18,4 +18,21 @@ let arrayToObject = function (array2D) {
 } 
 console.log(arrayToObject(car));
 console.log(arrayToObject(student));
-console.log(arrayToObject(fruits));
+console.log(arrayToObject(fruits));*/
+
+ const car= {
+    make : 'Ford',
+    model : 'Mustang',
+    year : 1964,
+    color : 'purple',
+  }
+  let properties = Object.keys(car);
+  console.log(properties);
+
+  let neWarray=[
+      [properties[0],car[properties[0]]],
+      [properties[1],car[properties[1]]], 
+      [properties[2],car[properties[2]]], 
+      [properties[3],car[properties[3]]]
+  ]
+console.log(neWarray);
