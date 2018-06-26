0d6e0f18641f7c883492172a9a99d9988887b4aa
Nicole Elgueta <nicole.m.elgueta@gmail.com>
2018-06-26 22:20:15 +0200

    arrayToObject

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
-const car = [['make', 'Ford'], ['model', 'Mustang'], ['year', 1964]]
-let object= {};
+const car = [['make', 'Ford'], ['model', 'Mustang'], ['year', 1964]];
+const student = [['name','nicole'],['age','36'],['city','villa alemana']];
+const fruits =[['apple','red'],['watermellon','green n pink'], ['limon', 'yellow']];
+
+/*let object = {};
 object['make'] = 'ford';
 object['model'] = 'mustang';
-object['year'] = '1964';
-console.log(object);
+object['year'] = '1964';**/
+//console.log(object);
 
-let arrayToObject = function(array2D){
+let arrayToObject = function (array2D) {
     let newObject = {};
-    for(let i=0; i<array2D.length ;i++){
+    for(let i=0; i < array2D.length ;i++){
        // nuevo Objeto va ser newObject[propiedades[i][0]]= valores[i][1]
-       newObject[array2D[i][0]]= array2D[[i][1]];
+       newObject [array2D[i][0]] = array2D[i][1];
     }
-    return newObject;
-}
+   return newObject;
+} 
+console.log(arrayToObject(car));
+console.log(arrayToObject(student));
+console.log(arrayToObject(fruits));
